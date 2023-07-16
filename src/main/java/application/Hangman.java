3378b8783a555935dad4fe4be94061f3d0a14c74
Seth Aker <saker132@gmail.com>
2023-07-16 14:20:24 -0500

    Completed Hangman Project

--- src/main/java/application/Hangman.java
+++ src/main/java/application/Hangman.java
@@ -3,24 +3,30 @@ package application;
 import ui.UserInput;
 import ui.UserOutput;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Hangman {
     UserOutput userOutput;
     UserInput userInput;
     FileUtilities fileUtilities;
 
-    public Hangman(){
+    public Hangman() {
         userInput = new UserInput();
         userOutput = new UserOutput();
         fileUtilities = new FileUtilities();
     }
 
-    public void run(){
+    public void run() {
 
         userOutput.displayWelcome();
 
         // 1. From the FileUtilities class, get a random word to use as the
         // secret word to guess.
-        // fileUtilities.readRandomLineFromFile()
+
+        String secretWord = fileUtilities.readRandomLineFromFile();
 
 
         // 2. Create a char array (or String) containing underscores for each
@@ -28,13 +34,22 @@ public class Hangman {
         // Secret word ...: giving (6 letters)
         // current guess .: ______ (6 underscores)
 
+        char[] underscoreArr = new char[secretWord.length()];
 
+        System.out.print("Secret word: ");
+
+        for (int i = 0; i < underscoreArr.length; i++) {
+            underscoreArr[i] = '_';
+            System.out.print(underscoreArr[i]);
+        }
         // 3. Create a variable (maybe a final variable?) to hold the
         // maximum number of guesses before losing
-
+        final int MAX_GUESSES = 10;
 
         // 4. Create a variable to keep track of the number of failed guesses
-
+        int failedGuesses = 0;
+        Scanner userInput = new Scanner(System.in);
+        List<String> guessedCharacters = new ArrayList<>();
 
         // 5. While the user hasn't reached the max number of failed guesses,
         //   * Print out the current letters the user has guessed. For example,
@@ -49,9 +64,53 @@ public class Hangman {
         //   * If the user has reached the maximum number of guesses without solving
         //     the secret word, print a message saying: try again.
 
-        // Challenge: add a theme mode for hangman words
-        // Challenge: add a hint option
-        // Challenge: create Wordle
+        while (failedGuesses < MAX_GUESSES) {
+            if(Arrays.toString(underscoreArr)
+                    .replace(", ", "")
+                    .replace("[", "")
+                    .replace("]", "")
+                    .equals(secretWord)) {
+                System.out.println("Congratulations! you guessed the secret word!");
+                break;
+            }
 
+            System.out.println("\n Please guess a letter, or the secret word: ");
+            String userGuess = userInput.nextLine().toLowerCase();
+
+            if (secretWord.contentEquals(userGuess)) {
+                System.out.println("Congratulations! you guessed the secret word!");
+                break;
+            } else if (secretWord.contains(userGuess)) {
+                for (int i = 0; i < underscoreArr.length; i++) {
+                    if (secretWord.charAt(i) == userGuess.charAt(0)) {
+                        underscoreArr[i] = userGuess.charAt(0);
+                    }
+                }
+                System.out.println("\nGood job! " + userGuess + " was in the secret word.\n");
+
+
+            } else {
+                failedGuesses++;
+                System.out.println("Sorry, that wasn't in the secret word.");
+
+            }
+            guessedCharacters.add(userGuess);
+            System.out.println("Remaining guesses: " + (MAX_GUESSES - failedGuesses));
+            System.out.println("Letters guessed so far: " + guessedCharacters);
+            System.out.println("Secret word: " + Arrays.toString(underscoreArr)
+                    .replace(", ", "")
+                    .replace("[", "")
+                    .replace("]", ""));
+
+
+            // Challenge: add a theme mode for hangman words
+            // Challenge: add a hint option
+            // Challenge: create Wordle
+
+        }
+        if(failedGuesses >= 10) {
+            System.out.println("Sorry, you lose. Try again.");
+            System.out.println("The secret word was: " + secretWord);
+        }
     }
 }
